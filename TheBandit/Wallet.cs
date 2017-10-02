f14a569d5707d30e249939cf60e73458b097bdae
Andreas Norberg <fiskkneipp@gmial.com>
2017-10-02 11:32:35 +0200

    Basic wallet done.

---
+++ TheBandit/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBandit
+{
+    class Wallet
+    {
+        private int money;
+
+        public int Money()
+        {
+            return money;
+        }
+
+        public void AddMoney(int add)
+        {
+            money += add;
+        }
+
+        public void SubtractMoney(int sub)
+        {
+            money -= sub;
+        }
+    }
+}
